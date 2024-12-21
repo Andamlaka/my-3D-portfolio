20dc445d0449816d8a96aea1c7a6f93738fb0996
Andamlaka <andargachweewawey@gmail.com>
2024-12-21 03:04:14 +0300

    set uu and navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Sections/Navbar'
+import Hero from './Sections/Hero'
+
+const App = () => {
+  return (
+    <div className=" max-w-7xl mx-auto ">
+     <Navbar />   
+     <Hero/>  
+    </div>
+  )
+}
+
+export default App
